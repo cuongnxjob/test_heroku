44b62a790839ca08170a442381ec381c848aa064
cuongnx2810 <cuongnx2810@gmail.com>
2020-03-30 18:49:19 +0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -118,7 +118,7 @@
 
         <!-- init block -->
         <script>
-            var client = "{!! json_encode($client) !!}";
+            var client = "{!! $client !!}";
             console.log(client);
             // var signedRequest = JSON.parse('<%=signedRequestJson%>');
         </script>
